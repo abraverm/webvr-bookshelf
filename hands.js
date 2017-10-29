41b8a7e8f6ad3abf585ed8cf7ed699d811df7342
Yotam Fromm <yotamen123@localhost.localdomain>
2017-10-29 16:52:39 +0200

    Hand animation

--- hands.js
+++ hands.js
@@ -1,17 +1,32 @@
-import. / handsjson.json as hands
 //create a line between joints
-var drawHand = function(scene){
-for (var i = 0; i < hands.length; i++) {
-  if (i < hands.length - 1)
-    drawLine(vectorFromJson(hands[i][0]), vectorFromJson(hands[i + 1][0]),scene)
-  for (var j = 0; j < hands[i].length; j++) {
-    if (j < hands.length - 1)
-      drawLine(vectorFromJson(hands[i][j]), vectorFromJson(hands[i][j + 1]),scene)
+var drawAnimation = function(scene, hand, jsonArray){
+  for(var i = 0; i<jsonArray.length;i++){
+    if(i != jsonArray.length-1){
+      setTimeout(function () {
+        updateHand(scene, hand, jsonArray[i]);
+    },(jsonArray[i+1]["timestamp"]-jsonArray[i]["timestamp"])*10000);
+
+    }
   }
 }
+var updateHand = function(scene, hand, jsonArray){
+  for (var i = 0; i < hand.length; i++) {
+    var point = jsonArray["pointables"][i]
+    var tip = vectorFromJson(point["tipPosition"])
+    var btip = vectorFromJson(point["btipPosition"])
+    var carp = vectorFromJson(point["carpPosition"])
+    var dip = vectorFromJson(point["dipPosition"])
+    var mcp = vectorFromJson(point["mcpPosition"])
+    var pip = vectorFromJson(point["pipPosition"])
+    hand[i]["a"] = updateLine(scene, hand[i]["a"], tip, btip);
+    hand[i]["b"] = updateLine(scene, hand[i]["b"], btip, dip);
+    hand[i]["c"] = updateLine(scene, hand[i]["c"], dip, pip);
+    hand[i]["d"] = updateLine(scene, hand[i]["d"], pip, mcp);
+    hand[i]["e"] = updateLine(scene, hand[i]["e"], mcp, carp);
+  }
 }
 
-var drawLine = function(pointStart, pointEnd,scene) {
+var updateLine = function(scene, line, pointStart, pointEnd){
   //Array of points to construct lines
   var myPoints = [
     pointStart,
@@ -19,12 +34,48 @@ var drawLine = function(pointStart, pointEnd,scene) {
   ];
 
   //Create lines with updatable parameter set to true for later changes
-  var lines = BABYLON.MeshBuilder.CreateLines("lines", {
+  var newline = BABYLON.MeshBuilder.CreateLines("lines", {
+    points: myPoints,
+    instance: line
+  }, scene);
+  return newline;
+}
+
+var drawHand = function(scene, hands) {
+  var hand = {};
+  for (var i = 0; i < hands["pointables"].length; i++) {
+    hand[i] = {};
+    var point = hands["pointables"][i]
+    var tip = vectorFromJson(point["tipPosition"])
+    var btip = vectorFromJson(point["btipPosition"])
+    var carp = vectorFromJson(point["carpPosition"])
+    var dip = vectorFromJson(point["dipPosition"])
+    var mcp = vectorFromJson(point["mcpPosition"])
+    var pip = vectorFromJson(point["pipPosition"])
+    hand[i]["a"] = drawLine(tip, btip, scene);
+    hand[i]["b"] = drawLine(btip, dip, scene);
+    hand[i]["c"] = drawLine(dip, pip, scene);
+    hand[i]["d"] = drawLine(pip, mcp, scene);
+    hand[i]["e"] = drawLine(mcp, carp, scene);
+  }
+  return hand;
+}
+
+var drawLine = function(pointStart, pointEnd, scene) {
+  //Array of points to construct lines
+  var myPoints = [
+    pointStart,
+    pointEnd
+  ];
+
+  //Create lines with updatable parameter set to true for later changes
+  var line = BABYLON.MeshBuilder.CreateLines("lines", {
     points: myPoints,
     updatable: true
   }, scene);
+  return line;
 }
-var vectorFromJson = function(var a){
-  var Vector = new BABYLON.Vector3(a[0],a[1],a[2]);
-  return Vector
+var vectorFromJson = function(a) {
+  var Vector = new BABYLON.Vector3(a[0], a[1], a[2]);
+  return Vector;
 }
