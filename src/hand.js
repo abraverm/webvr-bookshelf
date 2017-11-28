d08a0292332f381bb37c1c754e64fd0e0a990d06
Yotam Fromm <yotamen123@localhost.localdomain>
2017-11-28 12:18:42 +0200

    Hand rotation works

--- src/hand.js
+++ src/hand.js
@@ -1,6 +1,7 @@
 import Finger from './finger.js';
 import * as BABYLON from 'babylonjs';
 
+
 export default class Hand {
   constructor(mesh, skeleton) {
     this.pointables = {};
@@ -8,14 +9,24 @@ export default class Hand {
     this.fingers = {};
     this.colliders = {};
     this.mesh = mesh;
-    this.mesh.scaling = new BABYLON.Vector3(100,100,100);
+    this.rotation_z;
+    this.rotation_x;
+    this.mesh.scaling = new BABYLON.Vector3(100, 100, 100);
     this.mesh.position = new BABYLON.Vector3(0, 121, 0);
     this.skeleton = skeleton;
-    this.bones = {'thumb': {}, 'index': {}, 'middle': {}, 'ring': {}, 'pinky': {}, 'arm': null, 'palm': null};
+    this.bones = {
+      'thumb': {},
+      'index': {},
+      'middle': {},
+      'ring': {},
+      'pinky': {},
+      'arm': null,
+      'palm': null
+    };
     for (var i = 0, len = skeleton.bones.length; i < len; i++) {
       var bone = skeleton.bones[i];
-      bone.markAsDirty();
-      if (bone.name.split('-').length > 1){
+      //bone.markAsDirty();
+      if (bone.name.split('-').length > 1) {
         var bonetype = bone.name.split('-')[0];
         var bonename = bone.name.split('-')[1];
         this.bones[bonetype][bonename] = skeleton.bones[i];
@@ -25,35 +36,61 @@ export default class Hand {
     }
   }
 
-  toVector(a){
+  toVector(a) {
     //return new BABYLON.Vector3(a[0],a[1], a[2]);
-    return new BABYLON.Vector3(-a[0]/100, a[1]/100, a[2]/100);
+    return new BABYLON.Vector3(-a[0] / 100, a[1] / 100, a[2] / 100);
   }
-  updateFinger(finger, data){
-    if (data){
-      var n = new BABYLON.Vector3(-1,1,1);
+  updateFinger(finger, data) {
+    if (data) {
+      var n = new BABYLON.Vector3(-1, 1, 1);
       var carp = BABYLON.Vector3.FromArray(data['carpPosition']).scale(0.01).multiply(n); // joint 0
-      var mcp = BABYLON.Vector3.FromArray(data['mcpPosition']).scale(0.01).multiply(n);   // joint 1
-      var pip = BABYLON.Vector3.FromArray(data['pipPosition']).scale(0.01).multiply(n);   // joint 2
-      var dip = BABYLON.Vector3.FromArray(data['dipPosition']).scale(0.01).multiply(n);   // joint 3
-      var tip = BABYLON.Vector3.FromArray(data['tipPosition']).scale(0.01).multiply(n);   // joint 4
-      var metacarpal = {'pos': BABYLON.Vector3.Center(carp, mcp)};         // bone 0
-      var proximal = {'pos': BABYLON.Vector3.Center(mcp, pip)};            // bone 1
-      var middle = {'pos': BABYLON.Vector3.Center(pip, dip)};              // bone 2
-      var distal = {'pos': BABYLON.Vector3.Center(dip, tip)};              // bone 3
+      var mcp = BABYLON.Vector3.FromArray(data['mcpPosition']).scale(0.01).multiply(n); // joint 1
+      var pip = BABYLON.Vector3.FromArray(data['pipPosition']).scale(0.01).multiply(n); // joint 2
+      var dip = BABYLON.Vector3.FromArray(data['dipPosition']).scale(0.01).multiply(n); // joint 3
+      var tip = BABYLON.Vector3.FromArray(data['tipPosition']).scale(0.01).multiply(n); // joint 4
+      var metacarpal = {
+        'pos': BABYLON.Vector3.Center(carp, mcp)
+      }; // bone 0
+      var proximal = {
+        'pos': BABYLON.Vector3.Center(mcp, pip)
+      }; // bone 1
+      var middle = {
+        'pos': BABYLON.Vector3.Center(pip, dip)
+      }; // bone 2
+      var distal = {
+        'pos': BABYLON.Vector3.Center(dip, tip)
+      }; // bone 3
       metacarpal['length'] = data['bones'][0]['length']
       proximal['length'] = data['bones'][1]['length'];
-      proximal['scaleLength'] = proximal['length']/this.bones[finger]['proximal'].length;
+      proximal['scaleLength'] = proximal['length'] / this.bones[finger]['proximal'].length;
       middle['length'] = data['bones'][2]['length'];
       distal['length'] = data['bones'][3]['length'];
       var direction = this.toVector(data['direction']);
-      this.bones[finger]['distal'].setPosition(distal['pos']);
-      this.bones[finger]['middle'].setPosition(middle['pos']);
-      this.bones[finger]['proximal'].setPosition(proximal['pos']);
-     }
-  }
+      this.bones[finger]['distal'].position = distal['pos'];
 
+      this.bones[finger]['middle'].position = middle['pos'];
+
+      this.bones[finger]['proximal'].position = proximal['pos'];
+      if(finger!='thumb'){
+        this.bones[finger]['direction'].position = direction;
+      }
+    }
+  }
+  /*
+  rotateBones() {
+      for each(var bone in this.bones) {
+        var mat = bone.getLocalMatrix();
+        mat = mat.multiply(BABYLON.Matrix.RotateX(this.rotation_x);
+        mat = mat.multiply(BABYLON.Matrix.RotateZ(this.rotation_z);
+        bone.updateMatrix(mat);
+      }
+    }
+*/
   updateHand(scene, data) {
+    this.rotation_z = data.roll() * (-1);
+    this.rotation_x = data.pitch();
+    this.mesh.rotation.x = this.rotation_x
+    this.mesh.rotation.z = this.rotation_z
     this.updateFinger('thumb', data.thumb);
     this.updateFinger('index', data.indexFinger);
     this.updateFinger('middle', data.middleFinger);
@@ -61,30 +98,32 @@ export default class Hand {
     this.updateFinger('pinky', data.pinky);
     this.bones['palm'].setPosition(this.toVector(data['palmPosition']));
     this.bones['arm'].setPosition(this.toVector(data['palmPosition']));
+    //rotateBones();
   }
 
-  correctHand() {
-    for (var i = 0; i < this.pointables.length; i++) {
-      var point = this.pointables[i]
-      var finger = point['type']
-      var tip = point["tipPosition"]
-      var carp = point["carpPosition"]
-      var dip = point["dipPosition"]
-      var mcp = point["mcpPosition"]
-      var pip = point["pipPosition"]
-      if (this.fingers[finger]) {
-        this.fingers[finger].updateFinger(tip, dip, mcp, pip, carp)
-      } else {
-        this.fingers[finger] = new Finger(tip, dip, mcp, pip, carp)
-      }
-      for (var key in this.colliders) {
-        this.fingers[finger].registerCollider(this.colliders[key])
+  /*
+    correctHand() {
+      for (var i = 0; i < this.pointables.length; i++) {
+        var point = this.pointables[i]
+        var finger = point['type']
+        var tip = point["tipPosition"]
+        var carp = point["carpPosition"]
+        var dip = point["dipPosition"]
+        var mcp = point["mcpPosition"]
+        var pip = point["pipPosition"]
+        if (this.fingers[finger]) {
+          this.fingers[finger].updateFinger(tip, dip, mcp, pip, carp)
+        } else {
+          this.fingers[finger] = new Finger(tip, dip, mcp, pip, carp)
+        }
+        for (var key in this.colliders) {
+          this.fingers[finger].registerCollider(this.colliders[key])
+        }
       }
     }
-  }
-
+    */
   registerCollider(name, mesh) {
-    if (this.colliders[name] == null){
+    if (this.colliders[name] == null) {
       this.colliders[name] = mesh;
     }
   }
